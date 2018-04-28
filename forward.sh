9d8e9f98b24e0e8008207cf0e820014dbf7a6ce8
dashjuvi <diegovm14@gmail.com>
2018-04-28 23:22:44 +0200

    RandomScripts

---
+++ forward.sh
@@ -0,0 +1,6 @@
+#Iniciar previamente la Vbox o no funcionara
+ifconfig vboxnet0 192.168.56.1
+#Para permitir que pase el trafico de w7-64 por nuestro linux
+sysctl -w net.ipv4.ip_forward=1
+#Para que funcione elastic sin que pete el bootstrap
+sysctl -w vm.max_map_count=262144
