3e59607ee01b0c85de35d0bdef95853a0f718710
Vash <diegovm14@gmail.com>
2019-06-14 16:35:31 +0200

    Update forward.sh

--- forward.sh
+++ forward.sh
@@ -3,5 +3,6 @@
 ifconfig vboxnet0 192.168.56.1
 #Para permitir que pase el trafico de w7-64 por nuestro linux
 sysctl -w net.ipv4.ip_forward=1
+echo 1 | sudo tee -a /proc/sys/net/ipv4/ip_forward
 #Para que funcione elastic sin que pete el bootstrap
 sysctl -w vm.max_map_count=262144
